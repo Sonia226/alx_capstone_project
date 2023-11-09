c713a281344b6a8a435870ac5ea4e7e2effde004
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-11-08 18:27:58 -0800

    delete

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed= new typed(".text", {
-    strings: ["Frontend developer", "Web developer", "Digital marketing", "content marketing"],
-    typespeed: 100,
-    backspeed: 100,
-    backDelay: 1000,
-    loop: true
-});
